ee33378c751f6812920ba418164af52437998a90
Aswin110 <aswinashok110@gmail.com>
2024-01-10 19:06:33 +0530

    added some more data to the database

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const Localstrategy = require('passport-local').Strategy;
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const User = require('./models/user');
 
 var catalogRouter = require('./routes/catalog');
 var usersRouter = require('./routes/users');
@@ -25,7 +30,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+console.log(process.env.SESSION_SECRET);
+// app.use(session({}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
