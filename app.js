7aea8c3930f8f0518b5028cb91a4dc3e82e2a4be
Aswin110 <aswinashok110@gmail.com>
2024-01-16 13:15:11 +0530

    working with jwt for authentication

--- app.js
+++ app.js
@@ -3,20 +3,91 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 const session = require('express-session');
-const passport = require('passport');
-const Localstrategy = require('passport-local').Strategy;
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const User = require('./models/user');
 const cors = require('cors');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
 
 var catalogRouter = require('./routes/catalog');
 var usersRouter = require('./routes/users');
 
 dotenv.config();
+
+passport.use('login', 
+	new LocalStrategy(
+		{
+			usernameField: 'username', 
+			passwordField: 'password',
+		},
+		async (username, password, done) => {
+			try {
+				const user = await User.findOne({username});
+				if(!user) {
+					return done(null, false, {message:'User not found'});
+				}
+				const validate = await user.isValidPassword(password);
+				if(!validate) {
+					return done(null, false, {message:'Wrong password'});
+				}
+				return done(null, user, {message:'Logged in successfully'});
+			} catch(err) {
+				return done(err);
+			}
+		}
+	)
+);
+
+passport.use(
+	new JWTStrategy(
+		{
+			secretOrKey: process.env.SECRET,
+			jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+		},
+		async (token, done) => {
+			try {
+				return done(null, token.user);
+			} catch (error) {
+				done(error);
+			}
+		}
+	)
+);
+  
+passport.use(
+	'signup',
+	new LocalStrategy(
+		{
+			usernameField: 'username',
+			passwordField: 'password',
+		},
+		async (username, password, done) => {
+			try {
+				const user = await User.create(
+					{
+						first_name: req.body.first_name,
+						last_name: req.body.last_name,
+						username, 
+						password
+					}
+				);
+  
+				return done(null, user);
+			} catch (error) {
+				done(error);
+			}
+		}
+	)
+);
+  
+
 mongoose.set('strictQuery', false);
 const mongoDB = process.env.MONGODB_URI;
 
@@ -32,9 +103,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+// app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
--- controller/authController.js
+++ controller/authController.js
@@ -2,19 +2,36 @@ asyncHandler = require('express-async-handler');
 const Post = require('../models/post');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const { body, validationResult } = require('express-validator');
+const bcrypt =require('bcryptjs');
 
 exports.login_get = asyncHandler(async(req, res, next)=> {
 	res.send('login page GET');
 });
 
 exports.login_post = asyncHandler(async(req, res, next)=> {
-	const user = {
-		id: 1,
-		username:'AswinAshok',
-		email:'brad@gmail.com',
-	};
-	jwt.sign({user},'secretkey',{expiresIn:'30s'},(err, token) => {
-		res.json({token});
+	passport.authenticate('login', async (err, user, info) => {
+		try {
+			if (err || !user) {
+				const error = new Error('An error occurred.');
+	
+				return next(error);
+			}
+	
+			req.login(user, { session: false }, async (error) => {
+				if (error) return next(error);
+	
+				const body = { _id: user._id, username: user.username };
+				const token = jwt.sign({ user: body }, process.env.SECRET, {
+					expiresIn: '1d',
+				});
+	
+				return res.json({ token });
+			});
+		} catch (error) {
+			return next(error);
+		}
 	});
 });
 // FORMAT of token
@@ -31,13 +48,67 @@ exports.verifyToken = (req, res, next)=> {
 	}
 };
 
-exports.signup_get = asyncHandler(async(req, res, next)=> {
-	res.send('signup page GET');
+exports.signup_success = asyncHandler(async (req, res, next) => {
+	const body = { _id: req.user._id, username: req.user.username };
+	const token = jwt.sign({ user: body }, process.env.SECRET, {
+		expiresIn: '1d',
+	});
+	res.json({ token });
 });
 
-exports.signup_post = asyncHandler(async(req, res, next)=> {
-	res.send('signup page POST');
+exports.signup_get = asyncHandler( async (req, res, next) => {
+	res.send('Signup page GET');
 });
+
+exports.signup_post =[
+	body('first_name')
+		.trim()
+		.isString()
+		.isLength({ min: 1 })
+		.withMessage('First name is required'),
+
+	body('last_name')
+		.trim()
+		.isString()
+		.isLength({ min: 1 })
+		.withMessage('Last name is required'),
+
+	body('username')
+		.trim()
+		.isLength({ min: 1, max: 25 })
+		.withMessage('Username must be between 1 and 25 characters'),
+
+	body('password')
+		.isLength({ min: 1 })
+		.withMessage('Password is too short'),
+
+	body('confirmPassword')
+		.isLength({ min: 1 })
+		.withMessage('Passwords do not match'),
+		
+	asyncHandler(async(req, res, next)=>{
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			console.log(errors.array());
+		} else {
+			const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+			const user = new User({
+				first_name: req.body.first_name,
+				last_name: req.body.last_name,
+				username: req.body.username,
+				password: hashedPassword,
+			});
+
+			await user.save();
+			next();
+		}
+	}),
+
+	passport.authenticate(
+		'signup', { session: false }),
+]; 
 
 exports.logout_get = asyncHandler(async(req, res, next)=> {
 	res.send('logout page GET');
@@ -54,3 +125,15 @@ exports.Check_username = asyncHandler(async(req, res, next)=>{
 	}
 });
 
+
+
+// exports.logout_in = asyncHandler(async(req, res, next)=> {
+// 	const user = {
+// 		id: 1,
+// 		username:'AswinAshok',
+// 		email:'brad@gmail.com',
+// 	};
+// 	jwt.sign({user},'secretkey',{expiresIn:'30s'},(err, token) => {
+// 		res.json({token});
+// 	});
+// });
